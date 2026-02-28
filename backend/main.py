56b2346f5c1f18f2ae3674695b5f6d041a37641e
Izan <moncer35104@gmail.com>
2026-02-28 13:16:01 +0100

    feat(backend) script sql

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.sherlock.sherlock import router as search_router
 from src.cloud.googleDrive import router as cloud_router
+from contextlib import asynccontextmanager
+from src.db.interfazDB import crearCollection
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Todo lo que esté aquí se ejecuta AL ARRANCAR
+    crearCollection()
+    yield
+    # Todo lo que esté aquí se ejecuta AL APAGAR (opcional)
+    # qdrant_client.close()
 
+app = FastAPI(title="Mi App Vectorial", lifespan=lifespan)
 # Add CORS
 app.add_middleware(
     CORSMiddleware,
